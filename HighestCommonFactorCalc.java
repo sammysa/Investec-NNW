8e8b554873bc6a1ec8ecc96fe75179fe9ee8dbff
Sammy Phatlane <phatlanems@gmail.com>
2024-06-13 10:39:17 +0200

    Initialised Highest Common Factor Method

--- HighestCommonFactorCalc.java
+++ HighestCommonFactorCalc.java
@@ -1,7 +1,21 @@
 public class HighestCommonFactorCalc {
 
+    // Main Method that will run
     public static void main(String[] args)
     {
-        System.out.println("Highest Common Factor Calculator");
+        int[] suppliedNumbers = {56, 67, 64, 14, 76, 9, 31};
+
+        // System.out.println("Highest Common Factor Calculator Class"); Initialised to check that our class work!
+
+        System.out.println("The Highest Common Factor is: " + highestCommFactor(suppliedNumbers));
+
+    }
+
+    // Static Method that will be used to determine our highest common factor - Assesment Scenario 1
+    public static int highestCommFactor(int[] numberArray)
+    {
+        int commFact = 0;
+
+        return commFact;
     }
 }
