fe8a7b7c715190e1f0f6a428b66136888afeffd3
Sammy Phatlane <phatlanems@gmail.com>
2024-06-13 11:31:15 +0200

    Updated gitignore to exclude out directory

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -3,8 +3,8 @@ public class GreatestCommonDivisor {
     // Main Method that will run
     public static void main(String[] args)
     {
-        int[] suppliedNumbers = {56, 67, 64, 14, 76, 9, 31};
-
+        //int[] suppliedNumbers = {56, 67, 64, 14, 76, 9, 31};
+        int[] suppliedNumbers = {2, 8, 18, 44, 80};
         // System.out.println("Highest Common Factor Calculator Class"); Initialised to check that our class work!
 
         System.out.println("The Greatest Common Divisor is: " + commonDivisorCalc(suppliedNumbers));
@@ -14,10 +14,17 @@ public class GreatestCommonDivisor {
     // Static Method that will be used to determine our greatest common divisor - Assesment Scenario 1
     public static int commonDivisorCalc(int[] numberArray)
     {
-        int commFact = 1; // Starting at 1 to avoid unnecessary iterarions
-        boolean isCommonDivisor = false;
+        /* Step Process
+        1. Find the minumum number in the array
+        2. Check the common factor
+        3. Return the common factor
+         */
+
+        int highestCommFact = 1; // Starting at 1 to avoid unnecessary iterarions
+        boolean isCommonDivisor = true; //Initialise default
         int minNumber = Integer.MAX_VALUE; //maximum positive integer value that can be represented
 
+        //1.
         for(int number : numberArray) // Determine the minimum number in the array
         {
             if(number < minNumber)
@@ -26,10 +33,23 @@ public class GreatestCommonDivisor {
             }
         }
 
-        /* Step Process
-        1.
-         */
+        //2.
+        for(int i = 1; i <= minNumber; i++)
+        {
+            for(int numb : numberArray)
+            {
+                if(numb % i != 0)
+                {
+                    isCommonDivisor = false;
+                    break;
+                }
+            }
+            if(isCommonDivisor) // if isCommonDivisor is true
+            {
+                highestCommFact = i;
+            }
+        }
 
-        return commFact;
+        return highestCommFact;
     }
 }
