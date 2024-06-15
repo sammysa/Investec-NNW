2c52c8eaef33dcb2ed7aeebc6e3a2faa81d434b3
Sammy Phatlane <phatlanems@gmail.com>
2024-06-15 22:30:38 +0200

    Working on JSON Parser class to read JSON file

--- AddressesFormat.java
+++ AddressesFormat.java
@@ -1,2 +1,27 @@
+//import org.json.simple.*;
+//import org.json.simple.parser.*;
+//
+//import java.io.FileReader;
+
 public class AddressesFormat {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Reading JSON File.");
+        //readJsonFile("addresses.json");
+    }
+
+    public static void readJsonFile(String fileName)
+    {
+//        JSONParser parser = new JSONParser();
+//        try{
+//            Object object = parser.parse(new FileReader(fileName));
+//            JSONObject jsonObject = (JSONObject) object;
+//
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+
+    }
 }
--- investec-addresses/src/main/java/za/co/sammysa/AddressJsonReader.java
+++ investec-addresses/src/main/java/za/co/sammysa/AddressJsonReader.java
@@ -1,20 +1,45 @@
 package za.co.sammysa;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class AddressJsonReader
 {
     public static void main( String[] args )
     {
         System.out.println( "Address File Reader" );
+
         String filePath = "addresses.json";
+        readJsonFile(filePath);
+
     }
 
     public static void readJsonFile(String fileName)
     {
+        StringBuilder jsonContentBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                jsonContentBuilder.append(line);
+            }}catch(IOException e) //in case file can not be found/read
+            {
+                e.printStackTrace();
+            }
 
+        // Parse JSON
+        JSONArray jsonArray = new JSONArray(jsonContentBuilder.toString());
+
+        // Display JSON Contents
+        int index = 0;
+        while (index < jsonArray.length())
+        {
+            JSONObject address = jsonArray.getJSONObject(index);
+            System.out.println("Address: " + (index + 1) + ":");
+
+            index++;
+        }
     }
 }
