4b93b2459af12ea311e76be399282ebc9e560f0c
Sammy Phatlane <phatlanems@gmail.com>
2024-06-15 22:29:11 +0200

    Added target directory to gitignore

---
+++ investec-addresses/src/main/java/za/co/sammysa/AddressJsonReader.java
@@ -0,0 +1,20 @@
+package za.co.sammysa;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Address File Reader" );
+        String filePath = "addresses.json";
+    }
+
+    public static void readJsonFile(String fileName)
+    {
+
+    }
+}
